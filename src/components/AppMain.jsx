8693ea3a8dfd0fd60d25446ff3ab17e83750103d
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-11-12 19:57:45 +0100

    project done

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,15 +1,21 @@
+import Button from '../components/Button/Button'
+
 export default function AppMain() {
     return (
         <main id="debug">
-            <section className="card">
+            <section className="card col-3">
                 <div className="spaceimg_card c-gray2">
-                    <span>600x400</span>
+                    <span className="within_spaceimg ">600x400</span>
                 </div>
-                <h3>Titolo del Post</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut enim, voluptatem libero, reprehenderit aperiam quasi, ipsam voluptates nobis deserunt voluptatum sequi cumque quas minima nisi. Dicta delectus similique autem ex nemo! Laborum veniam, aspernatur incidunt molestias assumenda iusto, deserunt dicta eum officia fugit id. Optio temporibus nihil vero id adipisci
-                </p>
-                <button className="btn_card">leggi di più</button>
+                <div className="card_bottom">
+                    <h3>Titolo del Post</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut enim, voluptatem libero, reprehenderit aperiam quasi, ipsam voluptates nobis deserunt voluptatum sequi cumque quas minima nisi. Dicta delectus similique autem ex nemo! Laborum veniam, aspernatur incidunt molestias assumenda iusto, deserunt dicta eum officia fugit id. Optio temporibus nihil vero id adipisci
+                    </p>
+                    <div>
+                        <Button/>
+                    </div>
+                </div>
             </section>
         </main>
     );
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import style from './Button.module.css'
+
+export default function Button(){
+    return(
+        <button className={style.btn_card}>Leggi di più</button>
+    );
+}
